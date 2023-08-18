37af6ef3c4cd3db24ed5bc30de8c1603949df31d
Ashwin9714 <92547149+Ashwin9714@users.noreply.github.com>
2023-08-17 17:27:37 -0700

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -8,5 +8,4 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 supports 		 'ubuntu', ">= 12.04"
 supports     'centos', ">= 6.5"
 
-depends "build-essential"
 depends "git"
